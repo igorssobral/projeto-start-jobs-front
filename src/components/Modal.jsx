13b5b0a6f9327c84aec165e72ef490ab044c1f3b
igorssobral <igorgmz1421@gmail.com>
2024-12-09 21:42:57 -0300

    feat: add modal to recover password, login and registration

---
+++ src/components/Modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Modal = ({ isVisible, children }) => {
+  if (!isVisible) return null;
+
+  return (
+    <div
+      className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'
+      id='wrapper'
+      
+    >
+      <div className='md:w-[600px] w-[90%] mx-auto flex flex-col'>
+        <div className='bg-white p-2 rounded'>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
